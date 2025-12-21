70d5c54f745e989a602346bb41331367fede346a
DiverseBloom780 <98516443+DiverseBloom780@users.noreply.github.com>
2025-12-21 01:55:27 -0600

    Refactor authentication structures and constants
    Updated structures and constants for authentication requests.

--- src/core/libraries/np/np_auth.h
+++ src/core/libraries/np/np_auth.h
@@ -4,7 +4,7 @@
 #pragma once
 
 #include "common/types.h"
-#include "core/libraries/np/np_types.h"
+#include "core/libraries/np/nptypes.h"
 
 namespace Core::Loader {
 class SymbolsResolver;
@@ -12,47 +12,97 @@ class SymbolsResolver;
 
 namespace Libraries::Np::NpAuth {
 
-constexpr s32 ORBIS_NP_AUTH_REQUEST_LIMIT = 0x10;
-constexpr s32 ORBIS_NP_AUTH_REQUEST_ID_OFFSET = 0x10000000;
+// Constants for request limits and offsets
+constexpr s32 ORBISNPAUTHREQUESTLIMIT = 0x10;
+constexpr s32 ORBISNPAUTHREQUESTIDOFFSET = 0x10000000;
 
+// Structure for creating an asynchronous authentication request
 struct OrbisNpAuthCreateAsyncRequestParameter {
+    // Size of the parameter
     u64 size;
-    u64 cpu_affinity_mask;
-    s32 thread_priority;
-    u8 padding[4];
+
+    // CPU affinity mask
+    u64 cpuaffinitymask;
+
+    // Thread priority
+    s32 threadpriority;
+
+ // Padding for alignment
+ u8 padding[4]; 
 };
 
+// Structure for getting an authorization code
 struct OrbisNpAuthGetAuthorizationCodeParameter {
+    // Size of the parameter
     u64 size;
-    const OrbisNpOnlineId* online_id;
-    const OrbisNpClientId* client_id;
-    const char* scope;
+
+    // Online ID
+    const OrbisNpOnlineId onlineid;
+
+    // Client ID
+    const OrbisNpClientId clientid;
+
+    // Scope
+    const char scope;
 };
 
+// Structure for getting an authorization code (A version)
 struct OrbisNpAuthGetAuthorizationCodeParameterA {
+    // Size of the parameter
     u64 size;
-    s32 user_id;
+
+    // User ID
+    s32 userid;
+
+    // Padding for alignment
     u8 padding[4];
-    const OrbisNpClientId* client_id;
-    const char* scope;
+
+    // Client ID
+    const OrbisNpClientId clientid;
+
+    // Scope
+    const char scope;
 };
 
+// Structure for getting an ID token
 struct OrbisNpAuthGetIdTokenParameter {
+    // Size of the parameter
     u64 size;
-    const OrbisNpOnlineId* online_id;
-    const OrbisNpClientId* client_id;
-    const OrbisNpClientSecret* client_secret;
-    const char* scope;
+
+    // Online ID
+    const OrbisNpOnlineId onlineid;
+
+    // Client ID
+    const OrbisNpClientId clientid;
+
+    // Client secret
+    const OrbisNpClientSecret clientsecret;
+
+    // Scope
+    const char scope;
 };
 
+// Structure for getting an ID token (A version)
 struct OrbisNpAuthGetIdTokenParameterA {
+    // Size of the parameter
     u64 size;
-    s32 user_id;
+
+    // User ID
+    s32 userid;
+
+    // Padding for alignment
     u8 padding[4];
-    const OrbisNpClientId* client_id;
-    const OrbisNpClientSecret* client_secret;
-    const char* scope;
+
+    // Client ID
+    const OrbisNpClientId clientid;
+
+    // Client secret
+    const OrbisNpClientSecret client_secret;
+
+    // Scope
+    const char scope;
 };
 
-void RegisterLib(Core::Loader::SymbolsResolver* sym);
+// Function to register the library
+void RegisterLib(Core::Loader::SymbolsResolver* sym); 
 } // namespace Libraries::Np::NpAuth
